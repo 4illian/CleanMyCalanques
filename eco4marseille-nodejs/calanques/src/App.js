bfb3eb7ba27ec56a231c883b1ade1f521d99a7cb
Sserci <85498195+Sserci@users.noreply.github.com>
2021-11-08 18:34:26 +0100

    init docker

---
+++ eco4marseille-nodejs/calanques/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Routing from './Routes';
+
+function App() {
+  return (
+    <div>
+      <Routing />
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ eco4marseille-nodejs/calanques/src/Pages/Homes.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+import Map from "../Components/Map"
+
+function Home() {
+    return (
+        <>
+           
+            <div>
+             <Map />
+            </div>
+
+        </>
+    );
+}
+
+export default Home;
