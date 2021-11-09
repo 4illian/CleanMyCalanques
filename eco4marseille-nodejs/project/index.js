29019c19e6f6ea7e88032b14c3e7d6c0eb3955db
Sserci <85498195+Sserci@users.noreply.github.com>
2021-11-09 02:51:25 +0100

    crud user complet

--- eco4marseille-nodejs/project/index.js
+++ eco4marseille-nodejs/project/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const port = 8080;
 const cors = require("cors");
+const userController = require("./controller/user");
 
 var path = require("path");
 
@@ -12,3 +16,9 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.get("/users", userController.getAll);
+app.get("/users/:id", userController.findOneById);
+app.post("/users", userController.create);
+app.post("/login", userController.login);
+app.delete("/users/:id", userController.deleteUser);
